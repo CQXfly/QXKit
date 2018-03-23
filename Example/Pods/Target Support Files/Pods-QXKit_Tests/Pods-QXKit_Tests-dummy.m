557eb3af6a12521638d1c31d1684015520d58395
qingxu.chong <qingxu.chong@yintech.cn>
2018-03-23 16:08:01 +0800

    commit first

---
+++ Example/Pods/Target Support Files/Pods-QXKit_Tests/Pods-QXKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QXKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_QXKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/QXKit/QXKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QXKit : NSObject
+@end
+@implementation PodsDummy_QXKit
+@end
